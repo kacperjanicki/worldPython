d8c738d52e60498d7839bc60ace1e4ccb7654e3d
kacperjanicki <kacperekjanicki9@gmai.com>
2022-06-04 20:10:32 +0200

    event log works, tile detection works

--- Main.py
+++ Main.py
@@ -19,25 +19,39 @@ class Frames(object):
         size=200*self.query.get()
         newwin.geometry(f"{100+int(size)}x{size}")
         
-        def tooltip_win():
+        # navigate to choose organism tooltip window
+        def tooltip_win(pos):
+            print(pos)
             self.chooseOrganism()
+            
         root.withdraw()  ## hide main menu 
         display = Label(newwin, text="world size: " + self.query.get()) #getting parameter via query var
         world = World(self.query.get(), 2)
         label = Label(text='sample label')
         label.grid(row=int(self.query.get())+1,column=0)
-
+        
+        position_log=[]
         class Tile:
             def __init__(self, x,y):
                 self.x = x
                 self.y = y
             def button_command(self):
                 self.button['text'] = 'x'
-                tooltip_win()
+                if position_log.count((self.x,self.y)) < 1:      ##allows only one tooltip at the time (per tile)
+                    tooltip_win((self.x,self.y))
+                    position_log.append((self.x,self.y))
+                
+                print(position_log.count((self.x,self.y)))
+                    
+
+                    
             def create(self):
                 self.button = Button(newwin,text='',command=self.button_command, height=5, width=5)
                 self.button.grid(row=self.x,column=self.y)
-                
+        
+        global event_log
+        event_log = []   
+
         # tworzenie kafelkow
         for x in range(int(self.query.get())):
             for i in range(int(self.query.get())):
@@ -49,18 +63,45 @@ class Frames(object):
         choose=Toplevel(root)
         choose.geometry('200x400')
         choose.title('Choose organism:')
-        text_label=Label(choose,text='siema')
-        text_label.grid(row=0,column=0)
+        nazwy = ['Human','Antelope','Fox','Sheep','Turtle','Wolf','Belladona','Grass','Guarana','Sosnowsky Weed','Sow Thistle']
+
+        def funkcja(i):
+            wiadomosc = f"{i} has been added."
+            print(wiadomosc)
+            event_log.append(wiadomosc)
+            print(event_log)
+
+        # nie da sie tego zrobic for loopem (chyba)
+        button1= Button(choose,text=nazwy[0],command=lambda: funkcja(nazwy[0]))
+        button1.grid(row=0,column=0)
+        button2= Button(choose,text=nazwy[1],command=lambda: funkcja(nazwy[1]))
+        button2.grid(row=1,column=0)
+        button3= Button(choose,text=nazwy[2],command=lambda: funkcja(nazwy[2]))
+        button3.grid(row=3,column=0)
+        button4= Button(choose,text=nazwy[3],command=lambda: funkcja(nazwy[3]))
+        button4.grid(row=4,column=0)
+        button5= Button(choose,text=nazwy[4],command=lambda: funkcja(nazwy[4]))
+        button5.grid(row=5,column=0)
+        button6= Button(choose,text=nazwy[5],command=lambda: funkcja(nazwy[5]))
+        button6.grid(row=6,column=0)
+        button7= Button(choose,text=nazwy[6],command=lambda: funkcja(nazwy[6]))
+        button7.grid(row=7,column=0)
+        button8= Button(choose,text=nazwy[7],command=lambda: funkcja(nazwy[7]))
+        button8.grid(row=8,column=0)
+        button9= Button(choose,text=nazwy[8],command=lambda: funkcja(nazwy[9]))
+        button9.grid(row=9,column=0)
+        button10= Button(choose,text=nazwy[9],command=lambda: funkcja(nazwy[9]))
+        button10.grid(row=0,column=0)
+        button11= Button(choose,text=nazwy[10],command=lambda: funkcja(nazwy[10]))
+        button11.grid(row=10,column=0)
 
     def mainFrame(self,root):
         self.query = StringVar() #passing parameter via query var
-        
         def click():
             if len(self.query.get()) == 0:
                 print('error')
             else:
                 self.newFrame()
-        
         root.title('Main win')
         root.geometry("300x300") 
         root.resizable(0, 0)
