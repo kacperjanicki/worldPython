ae98c4947d7e4833a5337cf1be46d82ddd72362c
kacperjanicki <kacperekjanicki9@gmai.com>
2022-06-04 17:37:33 +0200

    displays tooltip window

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 
+from cgitb import text
 import tkinter
 # import pygame
 from world.World import World
@@ -10,18 +11,19 @@ from tkinter import *
                                     # print(data)
 
 class Frames(object):
+    
     def newFrame(self):
         newwin = Toplevel(root)
         user_data = self.query.get()
         newwin.title(f'size: {self.query.get()}')
         size=200*self.query.get()
         newwin.geometry(f"{100+int(size)}x{size}")
-
-         
+        
+        def tooltip_win():
+            self.chooseOrganism()
         root.withdraw()  ## hide main menu 
         display = Label(newwin, text="world size: " + self.query.get()) #getting parameter via query var
         world = World(self.query.get(), 2)
-
         label = Label(text='sample label')
         label.grid(row=int(self.query.get())+1,column=0)
 
@@ -30,39 +32,25 @@ class Frames(object):
                 self.x = x
                 self.y = y
             def button_command(self):
-                print(self.x,self.y)
-                # for i in button_list:
-                #     if i == (self.x,self.y):
-                #         this_button=button2[button_list.index(i)]
-                #         print(this_button.text)
                 self.button['text'] = 'x'
-                
-
-
-            global button_list,button2
-            button_list,button2=[],[]
+                tooltip_win()
             def create(self):
                 self.button = Button(newwin,text='',command=self.button_command, height=5, width=5)
                 self.button.grid(row=self.x,column=self.y)
-                button_list.append((self.x,self.y))
-                button2.append(self.button)
-                    
-
-        
+                
         # tworzenie kafelkow
         for x in range(int(self.query.get())):
             for i in range(int(self.query.get())):
                 Tile(0,i).create()
             for i in range(int(self.query.get())):
                 Tile(x,i).create()
-        # print(button_list)
-        
 
-
-            
-
-
-
+    def chooseOrganism(self):
+        choose=Toplevel(root)
+        choose.geometry('200x400')
+        choose.title('Choose organism:')
+        text_label=Label(choose,text='siema')
+        text_label.grid(row=0,column=0)
 
     def mainFrame(self,root):
         self.query = StringVar() #passing parameter via query var
@@ -86,7 +74,6 @@ class Frames(object):
         entry1 = Entry(root, textvariable=self.query)
         entry1.grid(row=1,column=0)
         
-
 if __name__ == "__main__":
     root = Tk()
     app = Frames()
